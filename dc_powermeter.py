7962102c07973322f54becb2122444f57d5c25d6
Lukáš Kaplan <lukas.kaplan@ispalliance.cz>
2020-09-08 22:41:53 +0200

    First commit

---
+++ dc_powermeter.py
@@ -0,0 +1,87 @@
+################################################################
+##                                                            ##
+##                       DC Powermeter                        ##
+##                                                            ##
+## Description:                                               ##
+##     This simple script reads current values                ##
+##     from Chinese HDHK current meter via RS485 ModBUS-RTU   ##
+##     and then send them to remote InfluxDB. Data can be     ##
+##     simply visualised by Grafana.                          ##
+## Author: Lukas Kaplan                                       ##
+## Email: lukas.kaplan@lkaplan.cz                             ##
+## License: BSD                                               ##
+## Version: 1.0                                               ##
+################################################################
+
+import time # for sleep(interval) 
+import datetime # for influxdb timestamp
+from influxdb import InfluxDBClient
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from configparser import ConfigParser
+  
+### Read the config file ###
+config_object = ConfigParser()
+config_object.read("/etc/powermeter.conf")
+influx = config_object["influx"]
+modbus = config_object["modbus"]
+ratio = config_object["ratio"]
+loop = config_object["loop"]
+
+# List of HDHK Current meter input names
+interface = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"]
+
+### ModBUS ###
+modbusclient = ModbusClient(
+    method = modbus["method"], 
+    port = modbus["port"], 
+    timeout = int(modbus["timeout"]), 
+    stopbits = int(modbus["stopbits"]), 
+    bytesize = int(modbus["bytesize"]),  
+    parity=modbus["parity"], 
+    baudrate = int(modbus["baudrate"])
+)
+modbusclient.connect()
+
+### INFLUX ###
+# Create the InfluxDB client object
+influxclient = InfluxDBClient(
+    influx["host"], 
+    influx["port"], 
+    influx["user"], 
+    influx["password"],
+    influx["dbname"]
+)
+
+# Run until you get a ctrl^c
+try:
+    while True:
+        timestamp=datetime.datetime.utcnow().isoformat()
+        request = modbusclient.read_holding_registers(address=0,count=56,unit=1)
+        #print(request.registers)
+
+        for i in range(0,len(interface)):
+            current = float(ratio[interface[i]]) * request.registers[8+i]
+            current = round(current, 2)
+            print(interface[i], current, "A")
+            # Create the JSON data structure
+            data = [
+            {
+              "measurement": influx["measurement"],
+                  "tags": {
+                      "interface": interface[i],
+                  },
+                  "time": timestamp,
+                  "fields": {
+                      "current" : current,
+                  }
+              }
+            ]
+            # Send the JSON data to InfluxDB
+            influxclient.write_points(data)
+        print('---')
+
+        # Wait until it's time to query again...
+        time.sleep(int(loop["interval"]))
+ 
+except KeyboardInterrupt:
+    pass
