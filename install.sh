e0e9b01bcdd880f418b1b1286ce10d958123944c
Lukas Kaplan <lukas.kaplan@lkaplan.cz>
2022-02-12 16:41:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ apt-get update
 apt-get upgrade
 apt-get install python3 python3-pip python3-pymodbus python3-influxdb
 
-cp ./dc_powermeter.py /usr/bin/
-cp ./dc_powermeter.conf /etc/
-cp ./dc_powermeter.service /lib/systemd/system/
+cp ./powermeter.py /usr/bin/
+cp ./powermeter.conf /etc/
+cp ./powermeter.service /lib/systemd/system/
 
 systemctl daemon-reload
-systemctl start dc_powermeter.service
-systemctl enable dc_powermeter.service
+systemctl start powermeter.service
+systemctl enable powermeter.service
 
-#systemctl status dc_powermeter.service
+#systemctl status powermeter.service
