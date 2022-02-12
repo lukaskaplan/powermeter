764864909b84e4223dea6122ea9865b89ac69391
Lukas Kaplan <lukas.kaplan@lkaplan.cz>
2022-02-12 20:23:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get upgrade
 apt-get install python3 python3-pip python3-pymodbus python3-influxdb
 
-cp ./powermeter.py /usr/bin/
+cp ./powermeter.py /usr/local/bin/
 cp ./powermeter.conf /etc/
 cp ./powermeter.service /lib/systemd/system/
 
